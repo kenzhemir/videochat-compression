6eb2eee057d3c6b6f66ccdfa227bfb39da2ef0c9
Alisher Iskakov <finesome@gmail.com>
2018-05-01 21:48:18 +0600

    Finally made client-server communication work

--- client.py
+++ client.py
@@ -1,69 +1,63 @@
-import socket
-import pickle
-import numpy as np
+import base64
 import cv2
-import matplotlib.pyplot as plt
+import pickle
+import socket
+import sys
+import time
 
 # constants
-BUF_SIZE = 4096
-HOSTNAME = "localhost"
+IMAGE_PATH = "image.jpg"
+HOST = "localhost"
 PORT = 8080
+BUF_SIZE = 8192
 
-class ClientSocket:
-    def __init__(self, hostname, port):
-        self.hostname = hostname
-        self.port = port
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# create a TCP/IP socket
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# connect the socket to server
+socket.connect((HOST, PORT))
 
-    def connect(self):
-        self.socket.connect((self.hostname, self.port))
+# create a named window
+cv2.namedWindow("frame")
+# create a capture from webcam
+cap = cv2.VideoCapture(0)
 
-    def send(self, msg):
-        print("Sending message to {}:{}".format(self.hostname, self.port))
-        data = pickle.dumps(msg)
-        self.socket.sendall(data)
+# get frame sizes
+width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+# send sizes to server
+width_message = "width: {}".format(int(width)).encode("utf-8")
+height_message = "height: {}".format(int(height)).encode("utf-8")
+print(width_message, height_message)
+# width_message = pickle.dumps(width_message)
+# height_message = pickle.dumps(height_message)
 
-    def receive(self, buf_size):
-        print("Waiting to receive a message of {} bytes".format(buf_size))
-        data = self.socket.recv(buf_size)
-        msg = pickle.loads(data)
-        return msg
+socket.sendall(width_message)
+time.sleep(0.1)
+socket.sendall(height_message)
 
-    def close(self):
-        self.socket.close()
+# camera loop
+while True:
+    # capture frame-by-frame
+    ret, frame = cap.read()
+    # convert to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # display the frame
+    cv2.imshow("frame", gray)
+    try:
+        # sending frame through socket
+        data = pickle.dumps(gray)
+        socket.sendall(data)
+        # time.sleep(0.01)
+        socket.sendall("end".encode("utf-8"))
+    except:
+        print("exception")
+        break
+    # exit loop on "q" key
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
 
-
-# read image from defined path
-image_path = "image.jpg"
-image = cv2.imread(image_path)
-
-# plt.imshow(image)
-# plt.show()
-
-# create an INET, STREAMing socket
-socket = ClientSocket(HOSTNAME, PORT)
-# now connect to the server on the defined port
-socket.connect()
-
-try:
-    # send image shape
-    sizemsg = "SIZE: {} {}".format(image.shape[0], image.shape[1])    
-    sizemsg = pickle.dumps(sizemsg)
-    socket.send(sizemsg)
-    print("SENT SIZE TO SERVER")
-
-    # receive answer
-    answer = socket.receive(BUF_SIZE)
-    answer = pickle.loads(answer)
-    print("ANSWER FROM SERVER: {}".format(answer))
-
-    if answer == "GOT SIZE":
-        # send image
-        print("NOW SENDING IMAGE TO SERVER")
-        data = pickle.dumps(image)        
-        sent = socket.send(data)
-        print("SENT: {}".format(sent))
-
-finally:
-    # close socket
-    socket.close()
+# close the socket
+socket.close()
+# release the webcam
+cap.release()
+cv2.destroyAllWindows()    
--- server.py
+++ server.py
@@ -1,96 +1,85 @@
-import socket
-import select
-import pickle
-import struct
-import threading
-import numpy as np
+import base64
 import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+import socket
+import sys
+import time
 
 # constants
-BUF_SIZE = 4096
-HOSTNAME = "localhost"
+CONNECTIONS = 1
+HOST = "localhost"
 PORT = 8080
+BUF_SIZE = 8192
 
-class ServerSocket:
-    def __init__(self, hostname, port):
-        self.hostname = hostname
-        self.port = port
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# create a TCP/IP socket
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# bind the socket to port
+server.bind((HOST, PORT))
+# listen for incoming connections
+server.listen(CONNECTIONS)
 
-    def start(self, connections):
-        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.socket.bind((self.hostname, self.port))
-        self.socket.listen(connections)
-
-    def accept(self):
-        (client, address) = self.socket.accept()
-        return (client, address)
-
-    def receive(self, buf_size):
-        return self.socket.recv(BUF_SIZE)
-
-
-
-# create a server socket
-server = ServerSocket(HOSTNAME, PORT)
-# start a server socket with 1 connections
-server.start(1)
-# print messages
-print("--- Started server at {}:{} ---".format(HOSTNAME, PORT))
-print("--- Waiting for the client ---")
-# accept a client; blocking call
-(client, address) = server.accept()
-# print message when client arrived
-print("--- Got a client ---")
+# create a named window
+cv2.namedWindow("server")
 
 result = []
+width, height, channels = 0, 0, 0
+
 state = 0
 
-# main loop
+prev = 0
+
 while True:
+    print("--- waiting for a connection ---")
+    client, client_address = server.accept()
     try:
-        print("--- Waiting for resource ---")
-        resource = server.receive(BUF_SIZE)
-        print("--- Got a response ---")
-        # if empty response
-        if resource == 0:
-            print("--- Empty response ---")
-            break
-        
-        if state == 0:
-            try:
-                data = pickle.loads(resource)
-            except:
-                print("--- Exception happened ---")
+        print("--- client from {} ---".format(client_address))
+        while True:
+            # print("--- getting data ---")
+            # get image size and decode
+            received = client.recv(BUF_SIZE)
+            if received:
+                # pass
+                if state == 0:                    
+                    temp = received.decode("utf-8")
+                    temp = temp.split()
+                    width = int(temp[1])
+                    print("GOT WIDTH : {}".format(width))
+                    state += 1
+                elif state == 1:
+                    temp = received.decode("utf-8")
+                    temp = temp.split()
+                    height = int(temp[1])
+                    print("GOT HEIGHT : {}".format(height))
+                    state += 1                                    
+                else:
+                    if received == b'end':
+                        curr = time.time()
+                        print("took {} time".format(1/abs(curr-prev)))
+                        # print("NEW FRAME")
+                        result_arr = pickle.loads(b"".join(result))
+                        image = np.frombuffer(result_arr, dtype=np.uint8)
+                        image = np.reshape(image, (height, width))
+                        cv2.imshow("server", image)
+                        # exit loop on "q" key
+                        if cv2.waitKey(1) & 0xFF == ord("q"):
+                            client.close()
+                            quit(0)
+                            break
 
-            if str(data).startswith("SIZE"):
-                msg = data.split()
-                shape = [int(msg[1]), int(msg[2])]
-                size = shape[0]*shape[1]
-                print("SHAPE: {}".format(shape))
-                client.send(pickle.dumps("GOT SIZE"))
-        else:
-            pass
-            # sborka baitov do saiza
-        print("--- Data accepted ---")
-
-        # if str(data).startswith("SIZE"):
-        #     print("GOT SIZE ON SERVER")
-        #     print(data)
-        #     answer = "GOT SIZE"
-        #     # answer = answer.encode("utf-8")
-        #     answer = pickle.dumps(answer)
-        #     clientsocket.sendall(answer)
-            
-        #     msg = data.split()
-        #     shape = [int(msg[1]), int(msg[2])]
-        #     print("SHAPE: {}".format(shape))            
-        # elif resource:
-        #     print("GOT IMAGE ON SERVER")
-        #     print(data)
-        #     result += data
-            
-    except KeyboardInterrupt:
-        server.close()
-        break
-
+                        # plt.imshow(image, cmap="gray")
+                        # plt.show()
+                        result = []
+                        prev = time.time()
+                        # quit(0)
+                    else:
+                        result.append(received)
+            else:
+                print("--- no more data from client ---")                                
+                print(width)
+                print(height)
+                print(channels)
+                break
+    finally:
+        client.close()
