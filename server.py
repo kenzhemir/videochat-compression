4ec193bd4227e51a0b0d0f91be548e3c3d21f840
Alisher Iskakov <finesome@gmail.com>
2018-05-01 18:04:36 +0600

    Wrapped server with a class

--- server.py
+++ server.py
@@ -1,33 +1,96 @@
 import socket
+import select
+import pickle
+import struct
 import threading
 import numpy as np
+import cv2
 
+# constants
 BUF_SIZE = 4096
-host = ""
-port = 8080
+HOSTNAME = "localhost"
+PORT = 8080
 
-# create an INET, STREAMing socket
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# bind the socket to a public host, and a well-known port
-serversocket.bind((host, port))
-# become a server socket
-serversocket.listen(5)
-print("Started server at {}:{}".format(host, port))
+class ServerSocket:
+    def __init__(self, hostname, port):
+        self.hostname = hostname
+        self.port = port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-(clientsocket, address) = serversocket.accept()
+    def start(self, connections):
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socket.bind((self.hostname, self.port))
+        self.socket.listen(connections)
 
-print("Client came")
+    def accept(self):
+        (client, address) = self.socket.accept()
+        return (client, address)
+
+    def receive(self, buf_size):
+        return self.socket.recv(BUF_SIZE)
+
+
+
+# create a server socket
+server = ServerSocket(HOSTNAME, PORT)
+# start a server socket with 1 connections
+server.start(1)
+# print messages
+print("--- Started server at {}:{} ---".format(HOSTNAME, PORT))
+print("--- Waiting for the client ---")
+# accept a client; blocking call
+(client, address) = server.accept()
+# print message when client arrived
+print("--- Got a client ---")
+
+result = []
+state = 0
+
+# main loop
 while True:
     try:
-        # accept connections from outside
-        # now do something with the clientsocket
-        # in this case, we'll pretend this is a threaded server
-        print("Waiting for resource")
-        resource = clientsocket.recv(BUF_SIZE)
-        if resource == bytes(0):
+        print("--- Waiting for resource ---")
+        resource = server.receive(BUF_SIZE)
+        print("--- Got a response ---")
+        # if empty response
+        if resource == 0:
+            print("--- Empty response ---")
             break
-        print("Resource accepted")
-        print(len(resource))
+        
+        if state == 0:
+            try:
+                data = pickle.loads(resource)
+            except:
+                print("--- Exception happened ---")
+
+            if str(data).startswith("SIZE"):
+                msg = data.split()
+                shape = [int(msg[1]), int(msg[2])]
+                size = shape[0]*shape[1]
+                print("SHAPE: {}".format(shape))
+                client.send(pickle.dumps("GOT SIZE"))
+        else:
+            pass
+            # sborka baitov do saiza
+        print("--- Data accepted ---")
+
+        # if str(data).startswith("SIZE"):
+        #     print("GOT SIZE ON SERVER")
+        #     print(data)
+        #     answer = "GOT SIZE"
+        #     # answer = answer.encode("utf-8")
+        #     answer = pickle.dumps(answer)
+        #     clientsocket.sendall(answer)
+            
+        #     msg = data.split()
+        #     shape = [int(msg[1]), int(msg[2])]
+        #     print("SHAPE: {}".format(shape))            
+        # elif resource:
+        #     print("GOT IMAGE ON SERVER")
+        #     print(data)
+        #     result += data
+            
     except KeyboardInterrupt:
-        serversocket.close()
+        server.close()
         break
+
